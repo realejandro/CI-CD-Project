47c43c891b1dee85fbeeef4468314d767e7af2d7
realejandro <realejandrocf@gmail.com>
2025-03-27 14:37:37 -0500

    repairing the file cleanDb.ts

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,11 +3,12 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
 
-    if (modelExists.length) {
+   let modelExists = await models[modelName]?.db?.db?.listCollections({
+      name: collectionName
+    }).toArray() || undefined ;
+
+    if (modelExists?.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
